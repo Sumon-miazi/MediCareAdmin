ff46c6b3b72b842fd48334918215e12fb50f97c4
Sumon-miazi <sumonmiazi59@gmail.com>
2020-11-22 00:02:23 +0600

    #added app widgets

---
+++ app/Widgets/Hospitals.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Widgets;
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
+use TCG\Voyager\Facades\Voyager;
+use TCG\Voyager\Widgets\BaseDimmer;
+
+class Hospitals extends BaseDimmer
+{
+    /**
+     * The configuration array.
+     *
+     * @var array
+     */
+    protected $config = [];
+
+    /**
+     * Treat this method as a controller action.
+     * Return view() or other content to display.
+     */
+    public function run()
+    {
+        $count = \App\Hospital::count();
+        $string = 'Hospitals';
+        $string_lower = Str::lower($string);
+        return view('voyager::dimmer', array_merge($this->config, [
+            'icon'   => 'voyager-group',
+            'title'  => "{$count} {$string}",
+            'text'   => "You have {$count}  {$string_lower} in your database. Click on button below to view all {$string_lower}",
+            'button' => [
+                'text' => "View all {$string}",
+                'link' => route('voyager.hospitals.index'),
+            ],
+            'image' => 'hospital_bg.jpg',
+        ]));
+    }
+
+    /**
+     * Determine if the widget should be displayed.
+     *
+     * @return bool
+     */
+    public function shouldBeDisplayed()
+    {
+        return Auth::user()->can('browse', Voyager::model('User'));
+    }
+}
